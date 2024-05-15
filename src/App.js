b7f5fdc1a2e31183fe2d91c5efdd75d175c5c00b
Noor-Abdelmuneim <157888524+Noor-Abdelmuneim@users.noreply.github.com>
2024-05-15 12:21:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import { deepOrange } from '@mui/material/colors';
+import LockClockOutlined from '@mui/icons-material/LockClockOutlined';
+import { Box, TextField } from '@mui/material';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Box sx={{
+          marginTop: '10px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+          <Avatar sx={{ bgcolor: deepOrange[500] }}><span class="material-icons-outlined">
+            <LockClockOutlined />
+          </span></Avatar>
+
+          <Typography variant="h5" component="h2">
+            Sign in
+          </Typography>
+          <Box>
+            <TextField id="outlined-basic" label="Email Address" variant="outlined" required margin='normal' fullWidth />
+            <TextField id="outlined-basic" label="Password" variant="outlined" type="password" required margin='normal' fullWidth />
+            <FormControlLabel control={<Checkbox defaultChecked />} label="Remember Me" />
+            <Button variant="contained" fullWidth sx={{ mt: 3, mb: 2 }}>SIGN IN</Button>
+            <Link href="#">Forget Password?</Link>
+          </Box>
+        </Box>
+
+      </Container >
+    </>
   );
 }
 
